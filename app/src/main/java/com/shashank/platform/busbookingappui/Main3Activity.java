3567d9b264deef5485e1eae2c651a5965d9e0fdb
codeurzebs <nguena.haupur@ictuniversity.edu.cm>
2023-08-25 18:17:50 +0100

    Update Main3Activity.java

--- app/src/main/java/com/shashank/platform/busbookingappui/Main3Activity.java
+++ app/src/main/java/com/shashank/platform/busbookingappui/Main3Activity.java
@@ -3,16 +3,23 @@ package com.shashank.platform.busbookingappui;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Main3Activity extends AppCompatActivity {
 
+    ImageView return_action;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main3);
+        return_action = findViewById(R.id.return_icon);
+        return_action.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+            startActivity(intent);
+        });
     }
 }
